a5ac5473dfc7b601333c7573daba404fc675d935
Gancho <gancho@Ganchos-MacBook-Pro.local>
2018-10-01 23:00:34 +0300

    Add comming soon block

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,16 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: pkg.bgName,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Caveat:400,700' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Marck+Script' }
     ]
   },
 
@@ -27,6 +29,7 @@ module.exports = {
   ** Global CSS
   */
   css: [
+    '@/assets/scss/main.scss'
   ],
 
   /*
@@ -42,7 +45,9 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
-    '@nuxtjs/bulma'
+    '@nuxtjs/bulma',
+    '@nuxtjs/font-awesome'
+    
   ],
   /*
   ** Axios module configuration
